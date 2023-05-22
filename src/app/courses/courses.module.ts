408d4afefa5150b9ac19cfc6c9f0d179a13c3341
davyods <113452525+davyods@users.noreply.github.com>
2023-05-22 13:23:01 -0300

    feat: add toolbar and change color (#1)
    * feat: add toolbar and change color
    
    * feat: looping and error message

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+
+
+
 
 
 @NgModule({
@@ -11,7 +16,9 @@ import { CoursesComponent } from './courses/courses.component';
   ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    AppMaterialModule,
+    SharedModule
   ]
 })
 export class CoursesModule { }
